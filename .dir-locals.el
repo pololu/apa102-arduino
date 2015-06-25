5db0c9f41605399facb4778a79b319bf92d80514
David (Pololu) <dev-david@pololu.com>
2015-06-25 13:35:33 -0700

    Initial commit.

---
+++ .dir-locals.el
@@ -0,0 +1,8 @@
+(
+  (c-default-style . "BSD")
+  (c-mode . ((c-basic-offset . 2) (tab-width . 2) (indent-tabs-mode . nil)))
+  (nil . ((fill-column . 80)))
+  ("examples" . (
+    (nil . ((c-basic-offset . 2) (tab-width . 8) (fill-column . 65)))
+  ))
+)
